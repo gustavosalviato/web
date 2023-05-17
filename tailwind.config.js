89bebfb7048bf7ffcc92ecf6ed7db6bb49f90d89
gustavosalviato <gustavosalviato8@gmail.com>
2023-05-17 12:32:58 -0300

    feat: front-end basic structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      alt: 'Bai Jamjuree'
+    }
   },
   plugins: [],
 }
